1d3a62a3a18b1249eef1f7a848edcb003e944007
Robin Lifshitz <robinsmac@Robins-MacBook-Pro.local>
2018-05-28 22:34:31 -0700

    updated minesweeper, still has bugs

--- minesweeper.js
+++ minesweeper.js
@@ -1,54 +1,82 @@
 const generatePlayerBoard = (numberOfRows, numberOfColumns) =>
-  {let board = [ ];
-  for(rows = 0; rows <= numberOfRows; rows++){
+  {const board = [ ];
+  for (let rows = 0; rows < numberOfRows; rows++) {
     //creates a new row for board
-    let row = [ ];
-    for(columns = 0; columns <= numberOfColumns; columns++){
+    let row = [];
+    for (columns = 0; columns < numberOfColumns; columns++) {
     //determines number of columns to be pushed to row array
-    row.push(' ');
+    row.push('');
   };
     //row array is pushed to board, this is on iteration of row for loop
     board.push(row);
   };
-  console.log(board);
+  return board;
 };
 //-------------------------------------------------------------------------------//
-const generateBombBoard = (numberOfRows, numberOfColumns, numberOfBombs) =>
-{
-    let board = [ ];
-    for(rows = 0; rows <= numberOfRows; rows++){
-      let row = [ ];
-      for(columns = 0; columns <= numberOfColumns; columns++){
-
-      row.push(null);
-      };
-
+const generateBombBoard = (numberOfRows, numberOfColumns, numberOfBombs) => {
+    const board = [ ];
+    for(let rows = 0; rows < numberOfRows; rows++){
+      const row = [ ];
+      for(columns = 0; columns < numberOfColumns; columns++){
+        row.push(null);
+      }
       board.push(row);
-    };
-
+    }
 
     let numberOfBombsPlaced = 0;
     //can potentially put bombs in the same place
-    while(numberOfBombsPlaced <= numberOfBombs){
+    while(numberOfBombsPlaced < numberOfBombs){
+      const randomRowIndex = Math.floor(numberOfRows * Math.random());
+      const randomColumnIndex = Math.floor(numberOfColumns * Math.random());
+      if(board[randomRowIndex][randomColumnIndex] !== "B"){
+        board[randomRowIndex][randomColumnIndex] = "B";
+        numberOfBombsPlaced++;
+      }
+    }
 
-      let randomRowIndex = Math.round(numberOfRows * Math.random());
-      let randomColumnIndex = Math.round(numberOfColumns * Math.random());
-      board[randomRowIndex][randomColumnIndex] = "B";
-      numberOfBombsPlaced ++;
-
-
-    };
-
-  return board;
+  console.log(board);
 };
-const printBoard = board => console.log(board.map(row => row.join(" | ")).join('/n'));
+//------------------------------------------------------------------------//
+//row & column index is selected by player
+const getNumberOfNeighborBombs = (bombBoard, rowIndex, columnIndex) => {
+  const neighborOffsets = [[-1, -1],[-1, 0],[-1, 1],[0, -1],[0, 1],[1, -1],[1, 0],[1, 1]];
+  const numberOfRows = bombBoard.length;
+  const numberOfColumns = bombBoard[0].length;
+  let numberOfBombs = 0;
 
+  neighborOffsets.forEach(offsets => {
+    const neighborRowIndex = rowIndex + offsets[0];
+    const neighborColumnIndex = columnIndex + offsets[1];
 
+    if(neighborRowIndex >= 0 && neighborRowIndex < numberOfRows && neighborColumnIndex >= 0 && neighborColumnIndex > numberOfColumns){
+      if(bombBoard[neighborRowIndex][neighborColumnIndex] === "B") {
+        numberOfBombs++;
+      }
+    }
+  });
+  return numberOfBombs;
+};
+//------------------------------------------------------------------------//
+const flipTile = (playerBoard, bombBoard, rowIndex, columnIndex) => {
 
-let playerBoard = generatePlayerBoard(3,4);
-let bombBoard = generateBombBoard(3,4,5);
+  if(playerBoard[rowIndex][columnIndex] !== " "){
+    console.log('This tile has already been flipped!');
+    return;
+  }else if(bombBoard[rowIndex][columnIndex] === 'B'){
+    playerBoard[rowIndex][columnIndex] = "B";
+  }else{
+    playerBoard[rowIndex][columnIndex] = getNumberOfNeighborBombs(bombBoard, rowIndex, columnIndex);
+  }
+};
 
-console.log('Player Board:');
+//prints a bomb or player board in correct structure
+const printBoard = board => console.log(board.map(row => row.join(" | ")).join('\n'));
+
+let playerBoard = generatePlayerBoard(3, 3);
+let bombBoard = generateBombBoard(3, 3, 3);
+console.log(getNumberOfNeighborBombs(bombBoard, 0, 0));
 printBoard(playerBoard);
-console.log('Bomb Board:')
 printBoard(bombBoard);
+flipTile(playerBoard, bombBoard, 0, 0);
+// // Flip different tiles based on bombBoard to see if neighbors work.
+printBoard(playerBoard);
